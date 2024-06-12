60da8f70293631a34072f46f0739df85c63a649f
Pranjal Rai <pranjalrai1@gmail.com>
2024-06-13 02:38:13 +0530

    2nd part completed - UI design

---
+++ frontend/src/components/messages/Message.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+
+const Message = () => {
+    return (
+        <div className='chat chat-end'>
+            <div className='chat-image avatar'>
+                <div className='w-10 rounded-full'>
+                    <img alt='Tailwind CSS chat bubble component' src='https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png' />
+                </div>
+            </div>
+            <div className={'chat-bubble text-white bg-blue-500'}>
+                Hi, What's up?
+            </div>
+            <div className='chat-footer opacity-50 text-xs flex gap-1 items-center'>
+                12:42
+            </div>
+        </div>
+    )
+}
+
+export default Message
+
+
+// STARTER CODE:
+
+// import React from 'react'
+
+// const Message = () => {
+//     return (
+//         <div className='chat chat-end'>
+//             <div className='chat-image avatar'>
+//                 <div className='w-10 rounded-full'>
+//                     <img alt='Tailwind CSS chat bubble component' src='https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png' />
+//                 </div>
+//             </div>
+//             <div className={'chat-bubble text-white bg-blue-500'}>
+//                 Hi, What's up?
+//             </div>
+//             <div className='chat-footer opacity-50 text-xs flex gap-1 items-center'>
+//                 12:42
+//             </div>
+//         </div>
+//     )
+// }
